faacacfce1aacbf87dee66f49872aaa9d75fd0aa
Matthieu BARRIERE <matthieu.barriere@sfr.fr>
2024-08-01 11:39:04 +0200

    Add styles to app

--- src/index.js
+++ src/index.js
@@ -6,11 +6,16 @@ import Survey from './pages/Survey';
 import Header from './components/Header';
 import Error from './components/Error';
 import Freelances from './pages/Freelances';
+import Results from './pages/Results';
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-    div {
-        font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    * {
+      font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+
+    body {
+      margin: 0;
     }
 `;
 
@@ -21,8 +26,9 @@ ReactDOM.render(
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/profils" element={<Freelances />} />
         <Route path="/survey/:questionNumber" element={<Survey />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/freelances" element={<Freelances />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </Router>
